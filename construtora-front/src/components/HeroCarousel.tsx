8da4e8efd9338a28934775f43ef8437319933d9e
Bruno Ibiapina <brunoibiapina@me.com>
2025-05-07 02:35:07 -0300

    start front

---
+++ construtora-front/src/components/HeroCarousel.tsx
@@ -0,0 +1,8 @@
+export default function HeroCarousel() {
+    return (
+        <div className="w-full h-64 bg-gray-200 flex items-center justify-center rounded mt-10">
+        <span className="text-2xl font-bold">Sua futura imagem aqui cachorra</span>
+      </div>
+    );
+  }
+  
--- construtora-front/src/pages/index.tsx
+++ construtora-front/src/pages/index.tsx
@@ -1,22 +1,41 @@
-import { useEffect } from "react";
-import { getEmpreendimentos } from "../api/empreendimentoApi"
+import { useEffect, useState } from "react";
+import { getEmpreendimentos, Empreendimento } from "../api/empreendimentoApi";
+import HeroCarousel from "../components/HeroCarousel";
+import EmpreendimentoCard from "../components/EmpreendimentoCard";
 
 export default function Home() {
+  const [empreendimentos, setEmpreendimentos] = useState<Empreendimento[]>([]);
 
   useEffect(() => {
     async function fetchData() {
-      const empreendimentos = await getEmpreendimentos();
-      console.log(empreendimentos)
-
+      const data = await getEmpreendimentos();
+      setEmpreendimentos(data);
     }
-
-    fetchData()
-  },[])
-
+    fetchData();
+  }, []);
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold">Home </h1>
+    // Container Geral
+    <main className="mx-auto p-8 max-w-6xl">
+      {/* HERO CAROUSEL */}
+      <HeroCarousel />
+
+      {/* TÍTULO */}
+      <h1 className="text-4xl font-bold mb-8 text-center mt-10">Empreendimentos</h1>
+
+      {/* GRID DE CARDS */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {empreendimentos.map((item) => (
+          <EmpreendimentoCard
+            key={item._id}
+            endereco={item.endereco || "Sem endereço"}
+            tamanho={item.tamanho || 0}
+            quartos={item.quantidadeQuartos || 0} cidade={""} tipo={""} vagas={0}          />
+        ))}
+        
+        
+      </div>
+      
     </main>
-  )
+  );
 }
