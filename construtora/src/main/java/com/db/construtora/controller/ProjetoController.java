f7c07c6a97a1c1b17ed7f52ec587e9d414dee8e1
Victor Gabriel <victorgabrielcc22@gmail.com>
2025-05-07 10:00:07 -0300

    RequestBody import

--- construtora/src/main/java/com/db/construtora/controller/ProjetoController.java
+++ construtora/src/main/java/com/db/construtora/controller/ProjetoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,6 @@ import com.db.construtora.dto.ProjetoCreationDto;
 import com.db.construtora.dto.ProjetoDto;
 import com.db.construtora.service.ProjetoService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping
